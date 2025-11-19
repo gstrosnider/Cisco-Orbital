468d7a8cfc2c2ef1ab1067f85997b269a27760bf
gstrosnider <53867437+gstrosnider@users.noreply.github.com>
2025-11-19 13:12:44 -0500

    Update orbitalnetworkscan.py

--- orbitalnetworkscan.py
+++ orbitalnetworkscan.py
@@ -1,20 +1,26 @@
-###Find all devices on the network and print IP and MAC Address
+###Find all devices on the IP_RANGE and print IP, MAC, and hostname
 from scapy.all import ARP, Ether, srp
+import socket
 
-###Adjust the target_ip to the network range to scan.
-target_ip = "192.168.1.1/24"
+target_ip = "{{ .IP_RANGE }}"
 arp = ARP(pdst=target_ip)
 ether = Ether(dst="ff:ff:ff:ff:ff:ff")
-packet = ether/arp
+packet = ether / arp
 
 result = srp(packet, timeout=3, verbose=0)[0]
 
 clients = []
 
 for sent, received in result:
-    clients.append({'ip': received.psrc, 'mac': received.hwsrc})
+    ip = received.psrc
+    mac = received.hwsrc
+    try:
+        hostname = socket.gethostbyaddr(ip)[0]
+    except socket.herror:
+        hostname = "N/A"
+    clients.append({'ip': ip, 'mac': mac, 'hostname': hostname})
 
 print("Found devices on the network:")
-print("IP" + " "*18+"MAC")
+print("{:<16} {:<20} {}".format("IP", "MAC", "HOSTNAME"))
 for client in clients:
-    print("{:16}    {}".format(client['ip'], client['mac']))
+    print("{:<16} {:<20} {}".format(client['ip'], client['mac'], client['hostname']))
